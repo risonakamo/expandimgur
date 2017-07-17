f40d1350534963b860c68e880c3337c4d0a085f5
risonakamo <hmykanae@gmail.com>
2017-07-16 23:41:21 -0400

    +album selector is tiles

--- expandimgur.js
+++ expandimgur.js
@@ -1,15 +1,15 @@
 document.head.insertAdjacentHTML("beforeend",`<link rel="stylesheet" href="${chrome.runtime.getURL("expandimgur.css")}">`);
 
-var timeUploadedButton=document.querySelector("#sort").querySelector(".options").children[1];
-var descendingButton=document.querySelector("#order").querySelector(".options").children[1];
+//click on time uploaded then descending
+var timeUploadedButton=document.querySelector("#sort .options").children[1];
+var descendingButton=document.querySelector("#order .options").children[1];
 document.addEventListener("keydown",(e)=>{
     if (e.key=="S" && e.ctrlKey==1 && e.shiftKey==1)
     {
-
         timeUploadedButton.click();
 
         setTimeout(()=>{
             descendingButton.click();
-        },100);
+        },150);
     }
 });
