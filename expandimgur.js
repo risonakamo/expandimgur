d935119c2f70d5137f7b01ca25549a1240668bf3
risonakamo <hmykanae@gmail.com>
2017-11-11 15:41:55 -0500

    change js into class change time upload key shortcut timing

--- expandimgur.js
+++ expandimgur.js
@@ -1,15 +1,29 @@
-document.head.insertAdjacentHTML("beforeend",`<link rel="stylesheet" href="${chrome.runtime.getURL("expandimgur.css")}">`);
-
-//click on time uploaded then descending
-var timeUploadedButton=document.querySelector("#sort .options").children[1];
-var descendingButton=document.querySelector("#order .options").children[1];
-document.addEventListener("keydown",(e)=>{
-    if (e.key=="S" && e.ctrlKey==1 && e.shiftKey==1)
+class _iexpand
+{
+    constructor()
     {
-        timeUploadedButton.click();
+        document.head.insertAdjacentHTML("beforeend",`<link rel="stylesheet" href="${chrome.runtime.getURL("expandimgur.css")}">`);
+
+        //click on time uploaded then descending
+        this.timeUploadedButton=document.querySelector("#sort .options").children[1];
+        this.descendingButton=document.querySelector("#order .options").children[1];
+
+        document.addEventListener("keydown",(e)=>{
+            if (e.key=="S" && e.ctrlKey==1 && e.shiftKey==1)
+            {
+                this.sortTimeDesc();
+            }
+        });
+    }
+
+    sortTimeDesc()
+    {
+        this.timeUploadedButton.click();
 
         setTimeout(()=>{
-            descendingButton.click();
-        },150);
+            this.descendingButton.click();
+        },200);
     }
-});
+}
+
+var iexpand=new _iexpand;
